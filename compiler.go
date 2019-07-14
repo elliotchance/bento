74bbca0a7e0e6158fd38b66cf4b16be58728e6ff
Elliot Chance <elliotchance@gmail.com>
2019-07-14 16:41:33 +1000

    Added inline "if" statements (#25)
    Sentences starting with `if` can be used to control the flow. An `if` sentence
    takes on of the following forms:
    
        if <condition>, <true>
    
        if <condition>, <true>, otherwise <false>
    
    Where `<condition>` is a simple comparison between two variables or values. Some
    examples are:
    
        name = "Bob"
        counter > 10
    
    All supported operators are:
    
    - `=` - Equal.
    - `!=` - Not equal.
    - `>` - Greater than.
    - `>=` - Greater than or equal.
    - `<` - Less than.
    - `<=` - Less than or equal.
    
    Values can only be compared when they are the same type. For example the
    following is not allowed, and will return an error:
    
        "123" = 123

--- compiler.go
+++ compiler.go
@@ -2,14 +2,10 @@ package main
 
 import "math/big"
 
-type Instruction struct {
-	Call string
-	Args []int
-}
-
 type CompiledFunction struct {
-	Variables    []interface{}
-	Instructions []Instruction
+	Variables         []interface{}
+	Instructions      []Instruction
+	InstructionOffset int
 }
 
 type CompiledProgram struct {
@@ -50,39 +46,128 @@ func CompileFunction(function *Function) *CompiledFunction {
 	}
 
 	// All of other constants are appended into the end.
-	for _, sentence := range function.Sentences {
-		instruction := Instruction{
-			Call: sentence.Syntax(),
-			Args: nil,
+	for _, statement := range function.Statements {
+		switch stmt := statement.(type) {
+		case *Sentence:
+			cf.Instructions = append(cf.Instructions,
+				compileSentence(cf, function, stmt))
+
+		case *If:
+			cf.Instructions = append(cf.Instructions,
+				compileIf(cf, function, stmt)...)
+
+		default:
+			panic(stmt)
 		}
-
-		// TODO: Check the syntax exists in the system or file.
-
-		for _, arg := range sentence.Args() {
-			switch a := arg.(type) {
-			case VariableReference:
-				for i, arg2 := range function.Variables {
-					if string(a) == arg2.Name {
-						instruction.Args = append(instruction.Args, i)
-						break
-					}
-				}
-
-				// TODO: handle bad variable name
-
-			case *string, *big.Rat:
-				instruction.Args = append(instruction.Args, len(cf.Variables))
-				cf.Variables = append(cf.Variables, a)
-
-			default:
-				// TODO: This shouldn't be possible, it can be removed when the
-				//  compiler is stable.
-				panic(arg)
-			}
-		}
-
-		cf.Instructions = append(cf.Instructions, instruction)
 	}
 
 	return cf
+}
+
+func compileSentence(cf *CompiledFunction, function *Function, sentence *Sentence) Instruction {
+	instruction := &CallInstruction{
+		Call: sentence.Syntax(),
+		Args: nil,
+	}
+
+	// TODO: Check the syntax exists in the system or file.
+
+	for _, arg := range sentence.Args() {
+		switch a := arg.(type) {
+		case VariableReference:
+			for i, arg2 := range function.Variables {
+				if string(a) == arg2.Name {
+					instruction.Args = append(instruction.Args, i)
+					break
+				}
+			}
+
+			// TODO: handle bad variable name
+
+		case *string, *big.Rat:
+			instruction.Args = append(instruction.Args, len(cf.Variables))
+			cf.Variables = append(cf.Variables, a)
+
+		default:
+			// TODO: This shouldn't be possible, it can be removed when the
+			//  compiler is stable.
+			panic(arg)
+		}
+	}
+
+	return instruction
+}
+
+func compileIf(cf *CompiledFunction, function *Function, ifStmt *If) []Instruction {
+	jumpInstruction := &ConditionJumpInstruction{
+		Operator: ifStmt.Condition.Operator,
+		True:     1,
+		False:    2,
+	}
+
+	if ifStmt.False != nil {
+		// This is to compensate for the added JumpInstruction that has to be
+		// added below.
+		jumpInstruction.False++
+	}
+
+	// TODO: This is duplicate code from compileSentence
+	switch a := ifStmt.Condition.Left.(type) {
+	case VariableReference:
+		for i, arg2 := range function.Variables {
+			if string(a) == arg2.Name {
+				jumpInstruction.Left = i
+				break
+			}
+		}
+
+	// TODO: handle bad variable name
+
+	case *string, *big.Rat:
+		jumpInstruction.Left = len(cf.Variables)
+		cf.Variables = append(cf.Variables, a)
+
+	default:
+		// TODO: This shouldn't be possible, it can be removed when the
+		//  compiler is stable.
+		panic(a)
+	}
+
+	// TODO: This is duplicate code from compileSentence (and above).
+	switch a := ifStmt.Condition.Right.(type) {
+	case VariableReference:
+		for i, arg2 := range function.Variables {
+			if string(a) == arg2.Name {
+				jumpInstruction.Right = i
+				break
+			}
+		}
+
+	// TODO: handle bad variable name
+
+	case *string, *big.Rat:
+		jumpInstruction.Right = len(cf.Variables)
+		cf.Variables = append(cf.Variables, a)
+
+	default:
+		// TODO: This shouldn't be possible, it can be removed when the
+		//  compiler is stable.
+		panic(a)
+	}
+
+	instructions := []Instruction{
+		jumpInstruction,
+		compileSentence(cf, function, ifStmt.True),
+	}
+
+	if ifStmt.False != nil {
+		instructions = append(instructions,
+			// This prevents the True case above from also running the else
+			// clause.
+			&JumpInstruction{Forward: 2},
+
+			compileSentence(cf, function, ifStmt.False))
+	}
+
+	return instructions
 }
